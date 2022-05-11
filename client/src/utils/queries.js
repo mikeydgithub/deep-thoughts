5a801392ee3921cae00fafc572a6e848b2860cea
Mike <ampgzo2@gmail.com>
2022-05-11 18:04:19 -0400

    fixed whitespace in queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -5,39 +5,39 @@ import { gql } from '@apollo/client';
 
 // we can import this query function by name and use it anywhere we need throughout the font end of the application.
 export const QUERY_THOUGHTS = gql`
-    query thoughts($username: String) {
-        thoughts(username: $username) {
-            _id
-            thoughtText
-            createdAt
-            username
-            reactionCount
-            reactions {
-                _id
-                createdAt
-                username
-                reactionBody
-            }
-        }
+  query thoughts($username: String) {
+    thoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
     }
+  }
 `;
 
 export const QUERY_THOUGHT = gql`
-    query thought($id: ID!) {
-        thought(_id: $id) {
-            _id
-            thoughtText
-            createdAt
-            username
-            reactionCount
-            reactions {
-                _id
-                createdAt
-                username
-                reactionBody
-            }
-        }    
-    }
+  query thought($id: ID!) {
+    thought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }    
+  }
 `;
 
 export const QUERY_USER = gql`
