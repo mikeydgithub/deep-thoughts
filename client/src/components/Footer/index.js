fbfcd0ebd86cbbcd93ca5300ab2216acf9b366cc
mikeydgithub <ampgzo2@gmail.com>
2022-06-03 12:44:20 -0400

    replaced files

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container">
-        &copy;{new Date().getFullYear()} by Mike Diamond
+        &copy;{new Date().getFullYear()} by Lernantino
       </div>
     </footer>
   );
